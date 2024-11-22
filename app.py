a18e3e3f8ff03022489a22cb154ec3d116985c44
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2024-11-22 10:49:29 -0300

    app.py improved with second deploy on Render

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import streamlit as st
 
 def hello_world():
+    # Return the first message
     return "Hello, World!"
 
+def deploy_message():
+    # Return the second message
+    return "**This is my second deploy, this one with Docker on Render**"
+
 def main():
+    # Display the first message
     st.write(hello_world())
+    
+    # Display the second message in bold and larger font
+    st.markdown(f"<h2 style='font-size: 30px; font-weight: bold;'>{deploy_message()}</h2>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
